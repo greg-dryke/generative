0ab51b6b582e9bcf275e337b8728a20d17f66c11
Greg Dryke <gdryke@github.com>
2023-01-06 07:39:14 -0600

    homescreen

--- pixel7.sh
+++ pixel7.sh
@@ -14,4 +14,5 @@ ls -1td lines_old/pixel7* | tail -n +10 | xargs rm
 mv pixel3.png lines_old/pixel7_$(date "+%Y-%m-%d_%H%M").png 2>/dev/null
 
 ## not being pushed?? Change here isn't
+# homescreen: ./generative -width 1440 -height 3120 -step 80 -lwidth 13 -output pixel7_homescreen.png -blankZone 0,190,1440,330
 ./generative -width 1440 -height 3120 -step 80 -lwidth 13 -output pixel7.png -blankZone 0,190,1440,250 -blankZone 140,400,680,970
