9e866d1136297912e5763e1e2f3f5a918d3d6201
Greg Dryke <gdryke@github.com>
2023-01-05 22:02:18 -0600

    blank zones

--- lines.go
+++ lines.go
@@ -10,7 +10,8 @@ import (
     "fmt"
     "flag"
     "strings"
-        crypto_rand "crypto/rand"
+    "strconv"
+    crypto_rand "crypto/rand"
     "encoding/binary"
     math_rand "math/rand"
 )
@@ -45,12 +46,43 @@ func init() {
     math_rand.Seed(int64(binary.LittleEndian.Uint64(b[:])))
 }
 
+type BlankZone struct {
+    xStart int
+    yStart int
+    xEnd int
+    yEnd int
+}
+
+type blankZoneArray []BlankZone
+
+func (i *blankZoneArray) String() string {
+    return "my string representation"
+}
+
+func (i *blankZoneArray) Set(value string) error {
+    var coordsStrings = strings.Split(value, ",")
+    coords := make([]int, len(coordsStrings))
+    var err error
+    for i := range coords {
+        coords[i], err = strconv.Atoi(coordsStrings[i])
+        if err != nil {
+            panic("Bad integer when processing blank zones")
+        }
+    }
+    var zone = BlankZone{coords[0], coords[1], coords[2], coords[3]}
+    *i = append(*i, zone)
+    return nil
+}
+
+var blankZones blankZoneArray
+
 func main() {
     var width = flag.Int("width", 200, "width of the image")
     var height = flag.Int("height", 100, "height of the image")
     var step = flag.Int("step", 20, "step for lines")
     var lineWidth = flag.Int("lwidth", 3, "width for lines")
     var outputFile = flag.String("output", "output.png", "name of the output png")
+    flag.Var(&blankZones, "blankZone", "2 points, comma separated, to remove art from. -blankZone 0,0,100,100")
     flag.Parse()
     // width := 1080
     // height := 2160
@@ -84,6 +116,14 @@ func main() {
                 fmt.Println("x:", x, "y:", y)
             }
             draw(img, c, x,y,*step,*step, *lineWidth)
+        }
+    }
+    blank :=  color.RGBA{0, 0, 0, 0x00}
+    for _, zone := range blankZones {
+        for x := zone.xStart; x < zone.xEnd; x+= 1 {
+            for y := zone.yStart; y < zone.yEnd; y+= 1 {
+                img.Set(x,y,blank);
+            }
         }
     }
 
--- pixel7.sh
+++ pixel7.sh
@@ -14,4 +14,4 @@ ls -1td lines_old/pixel7* | tail -n +10 | xargs rm
 mv pixel3.png lines_old/pixel7_$(date "+%Y-%m-%d_%H%M").png 2>/dev/null
 
 ## not being pushed?? Change here isn't
-./generative -width 1440 -height 3120 -step 80 -lwidth 13 -output pixel7.png
+./generative -width 1440 -height 3120 -step 80 -lwidth 13 -output pixel7.png -blankZone 0,0,1440,90 -blankZone 76,220,500,680
